a411accc9f31156d8eb457711e20e4b01ae8bce8
sashapet <sashapet@bb63b5ea-01e5-11df-886e-6fd961b10eda>
2010-02-23 20:56:02 +0000

    last changes to cupcake port, before I switch to eclair.. N19 specific drivers and apps are not ported (or copied) yet, but I'd rather do that on eclair

--- trunk/platform/build/target/product/SciphoneN19.mk
+++ trunk/platform/build/target/product/SciphoneN19.mk
@@ -2,23 +2,7 @@
 # It includes the base Android platform. If you need Google-specific features,
 # you should derive from generic_with_google.mk
 
-PRODUCT_PACKAGES := \
-    AlarmClock \
-    AlarmProvider \
-    Calendar \
-    Camera \
-    DrmProvider \
-    LatinIME \
-    Mms \
-    Music \
-    Settings \
-    Sync \
-    Updater \
-    CalendarProvider \
-    SubscribedFeedsProvider \
-    SyncProvider
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
 
 # Overrides
 PRODUCT_BRAND := Sciphone
