d262cda9743a4e2f75546ac6fed6d633368e33c5
sashapet <sashapet@bb63b5ea-01e5-11df-886e-6fd961b10eda>
2010-03-22 07:00:55 +0000

    keypad fix... d'uh forgot to checking earlier

--- branches/eclair_platform_patch/build/target/board/N19/AndroidBoard.mk
+++ branches/eclair_platform_patch/build/target/board/N19/AndroidBoard.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-file := $(TARGET_OUT_KEYLAYOUT)/tuttle2.kl
+file := $(TARGET_OUT_KEYLAYOUT)/keypad.kl
 ALL_PREBUILT += $(file)
 $(file) : $(LOCAL_PATH)/keypad.kl | $(ACP)
 	$(transform-prebuilt-to-target)
